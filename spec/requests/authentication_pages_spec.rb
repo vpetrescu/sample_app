a1bd71436e06daf7d250fc1a6d337a582a3492ed
Viviana Petrescu <petrescu.viviana@gmail.com>
2014-08-27 21:12:19 +0200

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,6 +26,7 @@ describe "Authentication" do
   describe "with valid information" do
     let(:user) { FactoryGirl.create(:user) }
     before do
+      visit signin_path
       fill_in "Email",  with: user.email.upcase
       fill_in "Password", with: user.password
       click_button "Sign in"
@@ -36,5 +37,10 @@ describe "Authentication" do
     it { should have_link('Sign out', href: signout_path) }
     it { should_not have_link('Sign in', href: signin_path) }
 
+    describe "followed by signout" do
+      before { click_link "Sign out" }
+      it { should have_link('Sign in') }
+    end
+    
   end
 end
